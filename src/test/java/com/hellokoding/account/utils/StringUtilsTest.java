9ddd3e9b149eba6684395fdeaea40dba9ec92f5c
akm200 <km.lu@live.cn>
2018-02-08 16:06:28 +0800

    add test case

---
+++ src/test/java/com/hellokoding/account/utils/StringUtilsTest.java
@@ -0,0 +1,16 @@
+package com.hellokoding.account.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+	
+	@Test
+	public void testIsEmpty() {
+		assertFalse(StringUtils.isEmpty("TEST"));
+		assertTrue(StringUtils.isEmpty(null));
+	}
+
+}
